00689a659465f3cd58dd4106201b5a7f911aeae4
Frankly Developing <frankly.developing@gmail.com>
2024-08-09 14:44:13 +0200

    TDDed new Card model

---
+++ CardGameMicroservicesTest/Models/CardTests.cs
@@ -0,0 +1,54 @@
+﻿using FluentAssertions;
+
+namespace CardGameMicroservicesTest.Models;
+
+public class CardTests
+{
+    [Theory]
+    [InlineData(Rank.Two, Suit.Heart, "2H")]
+    [InlineData(Rank.Three, Suit.Heart, "3H")]
+    [InlineData(Rank.Three, Suit.Diamond, "3D")]
+    [InlineData(Rank.Jack, Suit.Diamond, "JD")]
+    [InlineData(Rank.King, Suit.Diamond, "KD")]
+    public void CardToString(Rank rank, Suit suit, string expected)
+    {
+        var card = new Card(rank, suit);
+        card.ToString().Should().Be(expected);
+    }
+    
+    [Theory]
+    [InlineData(Rank.Two, Suit.Heart, "2H")]
+    [InlineData(Rank.Three, Suit.Heart, "3H")]
+    [InlineData(Rank.Three, Suit.Diamond, "3D")]
+    [InlineData(Rank.Jack, Suit.Diamond, "JD")]
+    [InlineData(Rank.King, Suit.Diamond, "KD")]
+    [InlineData(Rank.Ten, Suit.Diamond, "10D")]
+    public void CardFromString(Rank rank, Suit suit, string input)
+    {
+        var card = Card.From(input);
+        card.Rank.Should().Be(rank);
+        card.Suit.Should().Be(suit);
+    }
+    
+    [Theory]
+    [InlineData(Rank.Two, Suit.Heart, "2h")]
+    [InlineData(Rank.Three, Suit.Heart, "3h")]
+    [InlineData(Rank.Three, Suit.Diamond, "3d")]
+    [InlineData(Rank.Jack, Suit.Diamond, "jd")]
+    [InlineData(Rank.King, Suit.Diamond, "kd")]
+    [InlineData(Rank.Ten, Suit.Diamond, "10d")]
+    public void CardFromLowercaseString(Rank rank, Suit suit, string input)
+    {
+        var card = Card.From(input);
+        card.Rank.Should().Be(rank);
+        card.Suit.Should().Be(suit);
+    }
+
+    [Fact]
+    public void InvalidInputForCardCreation()
+    {
+        var action = () => Card.From("nonsense");
+        action.Should().Throw<ArgumentException>();
+    } 
+    
+}
---
+++ PokerGameCheckerMicroservice/Models/Card.cs
@@ -0,0 +1,58 @@
+﻿using System.ComponentModel;
+using Microsoft.OpenApi.Extensions;
+
+namespace CardGameMicroservicesTest.Models;
+
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+public enum Rank
+{
+    [Description("2")] Two,
+    [Description("3")] Three,
+    [Description("4")] Four,
+    [Description("5")] Five,
+    [Description("6")] Six,
+    [Description("7")] Seven,
+    [Description("8")] Eight,
+    [Description("9")] Nine,
+    [Description("10")] Ten,
+    [Description("J")] Jack,
+    [Description("Q")] Queen,
+    [Description("K")] King,
+    [Description("A")] Ace
+}
+
+public enum Suit
+{
+    [Description("H")] Heart,
+    [Description("D")] Diamond,
+    [Description("C")] Club,
+    [Description("S")] Spade
+}
+
+public record Card(Rank Rank, Suit Suit)
+{
+    public override string ToString()
+    {
+        return Rank.GetAttributeOfType<DescriptionAttribute>().Description
+               + Suit.GetAttributeOfType<DescriptionAttribute>().Description;
+    }
+
+    public static Card From(string input)
+    {
+        var rank = Enum.GetValues<Rank>()
+            .FirstOrDefault(s =>
+                input.StartsWith(s.GetAttributeOfType<DescriptionAttribute>().Description, StringComparison.OrdinalIgnoreCase));
+        var suit = Enum.GetValues<Suit>()
+            .FirstOrDefault(s =>
+                input.EndsWith(s.GetAttributeOfType<DescriptionAttribute>().Description, StringComparison.OrdinalIgnoreCase));
+        // nulls would default to 0 enums
+        if (rank == 0 && !input.StartsWith("2")
+            || (suit == 0 && !input.EndsWith("H", StringComparison.OrdinalIgnoreCase)))
+        {
+            throw new ArgumentException(nameof(input));
+        }
+        return new Card(rank, suit);
+    }
+}
+
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
